c5053594893eed6e5cf468ed36571ebd76b906bc
Simon Strom <simon.strom@gmail.com>
2016-09-27 11:33:45 +0200

    add a square function

--- run.js
+++ run.js
@@ -1,1 +1,9 @@
 console.log('This is printed from our test application')
+
+var number = 10
+
+function square(number) {
+  return number * number
+}
+
+console.log('a square of', number, ' is ', square(number))
