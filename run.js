329b00e1e2b5339a222aab982910a01c5f70d809
Simon Strom <simon.strom@gmail.com>
2016-09-28 11:29:08 +0200

    change square to twice

--- run.js
+++ run.js
@@ -2,8 +2,8 @@ console.log('This is printed from our test application')
 
 var number = 10
 
-function square(number) {
-  return number * number
+function twice(number) {
+  return number + number
 }
 
-console.log('a square of', number, ' is ', square(number))
+console.log('2 times of', number, ' is ', twice(number))
